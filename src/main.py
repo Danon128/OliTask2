34d8c4de871ef8fee9729d25ac02b687dd4eb1f6
Danon128 <157307836+Danon128@users.noreply.github.com>
2024-01-27 18:56:53 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,47 +2,53 @@ import cv2 as cv
 import numpy as np
 import math
 
+
 def img_rotate(fname, fname_pr):
     image_original = cv.imread(fname, cv.IMREAD_GRAYSCALE)
     final = cv.imread(fname, cv.IMREAD_COLOR)
 
-    #очистка от шумов
-    image_clean = cv.adaptiveThreshold(image_original, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 10)
-    cv.imwrite('clean.jpg', image_clean)
-    
-    #изменение размера
-    final_wide = 200
-    r = float(final_wide) / image_original.shape[1]
-    dim = (final_wide, int(image_original.shape[0] * r))
-    image_resized = cv.resize(image_clean, dim, interpolation = cv.INTER_AREA)
-    cv.imwrite('resized.jpg', image_resized)
+    # очистка от шумов
+    image_clean = cv.adaptiveThreshold(
+        image_original, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 10
+    )
+    cv.imwrite("clean.jpg", image_clean)
 
-    #выделение границ Кэнни
+    # выделение границ Кэнни
     image_conf = cv.Canny(image_original, 850, 900)
-    cv.imwrite('conf.jpg', image_conf)
+    cv.imwrite("conf.jpg", image_conf)
 
-    cdstP = cv.cvtColor(image_conf, cv.COLOR_GRAY2BGR)
-
-    #преобразование Хафа и отсечение линий
+    # преобразование Хафа для выделения прямых линий и отсечение линий
+    image_conf_lines = cv.cvtColor(image_conf, cv.COLOR_GRAY2BGR)
     lines_angles = []
-    linesP = cv.HoughLinesP(image_conf, 1, np.pi / 180, 100, None, 20, 10)
-    if linesP is not None:
-        for i in range(0, len(linesP)):
-            l = linesP[i][0]
-            if l[2]-l[0] == 0:
-                continue;
-            deg = math.degrees(math.atan((l[3]-l[1])/(l[2]-l[0])))
+    lines_hough = cv.HoughLinesP(image_conf, 1, np.pi / 180, 100, None, 20, 10)
+    if lines_hough is not None:
+        for i in range(0, len(lines_hough)):
+            line = lines_hough[i][0]
+            if line[2] - line[0] == 0:
+                continue
+            deg = math.degrees(math.atan((line[3] - line[1]) / (line[2] - line[0])))
             if -40 < deg < 40:
-                cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 1, cv.LINE_AA) 
+                cv.line(
+                    image_conf_lines,
+                    (line[0], line[1]),
+                    (line[2], line[3]),
+                    (0, 0, 255),
+                    1,
+                    cv.LINE_AA,
+                )
                 lines_angles.append(deg)
 
-    cv.imwrite('dstWithLinesP.jpg', cdstP)
+    cv.imwrite("image_conf_lines.jpg", image_conf_lines)
     rotation_angle = np.mean(lines_angles)
-    print(f'rotation angle = {rotation_angle}')
+    print(f"rotation angle = {rotation_angle}")
     (h, w) = image_original.shape[:2]
     center = (w / 2, h / 2)
-    M = cv.getRotationMatrix2D(center, rotation_angle, 1.0)
-    binrotated = cv.warpAffine(image_original, M, (w, h), borderMode=cv.BORDER_REPLICATE)
-    rotated = cv.warpAffine(final, M, (w, h), borderMode=cv.BORDER_REPLICATE)
+    image_rotation_matrix = cv.getRotationMatrix2D(center, rotation_angle, 1.0)
+    image_rotated = cv.warpAffine(
+        image_original, image_rotation_matrix, (w, h), borderMode=cv.BORDER_REPLICATE
+    )
+    cv.imwrite(fname_pr, image_rotated)
 
-    cv.imwrite(fname_pr, rotated)
+
+if __name__ == "__main__":
+    img_rotate("sample3.jpg", "result.jpg")
